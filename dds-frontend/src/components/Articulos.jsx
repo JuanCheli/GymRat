b0cf27014dcc01b76f69ff879a63ae99f733087e
Juan Cheli <juancheli29@gmail.com>
2024-06-21 19:40:16 -0300

    FRONT - ETAPA 7

--- dds-frontend/src/components/Articulos.jsx
+++ dds-frontend/src/components/Articulos.jsx
@@ -5,6 +5,7 @@ import ArticulosListado from "./ArticulosListado";
 import ArticulosRegistro from "./ArticulosRegistro";
 import { articulosService } from "../../services/articulos.services";
 import { articulosFamiliasService } from "../../services/articulosFamilias.services";
+import modalDialogService from "../../services/modalDialog.services";
 
 function Articulos() {
   const TituloAccionABMC = {
@@ -43,12 +44,14 @@ function Articulos() {
     else {
       _pagina = Pagina;
     }
-
+    modalDialogService.BloquearPantalla(true);
     const data = await articulosService.Buscar(Nombre, Activo, _pagina);
+    modalDialogService.BloquearPantalla(false);
+
     setItems(data.Items);
     setRegistrosTotal(data.RegistrosTotal);
 
-    //generar array de las páginas para mostrar en select del paginador
+    //generar array de las paginas para mostrar en select del paginador
     const arrPaginas = [];
     for (let i = 1; i <= Math.ceil(data.RegistrosTotal / 10); i++) {
       arrPaginas.push(i);
@@ -68,7 +71,7 @@ function Articulos() {
 
   function Modificar(item) {
     if (!item.Activo) {
-      alert("No puede modificarse un registro Inactivo.");
+      modalDialogService.Alert("No puede modificarse un registro Inactivo.");
       return;
     }
     BuscarPorId(item, "M"); // paso la accionABMC pq es asincrono la busqueda y luego de ejecutarse quiero cambiar el estado accionABMC
@@ -95,15 +98,18 @@ function Articulos() {
   }
 
   async function ActivarDesactivar(item) {
-    const resp = window.confirm(
-      "Está seguro que quiere " +
+    modalDialogService.Confirm(
+      "Esta seguro que quiere " +
         (item.Activo ? "desactivar" : "activar") +
-        " el registro?"
+        " el registro?",
+      undefined,
+      undefined,
+      undefined,
+      async () => {
+        await articulosService.ActivarDesactivar(item);
+        await Buscar();
+      }
     );
-    if (resp) {
-      await articulosService.ActivarDesactivar(item);
-      await Buscar();
-    }
   }
 
   async function Grabar(item) {
