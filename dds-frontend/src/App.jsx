29df10896b5eeb8ff73b7749894eac2288bcf661
Juan Cheli <juancheli29@gmail.com>
2024-06-21 19:01:33 -0300

    FRONT - ETAPA 4

--- dds-frontend/src/App.jsx
+++ dds-frontend/src/App.jsx
@@ -4,6 +4,8 @@ import { Inicio } from "./components/Inicio";
 import { ArticulosFamilias } from "./components/ArticulosFamilias";
 import {Menu} from "./components/Menu";
 import { Footer } from "./components/Footer";
+import { Articulos } from "./components/articulos/Articulos";
+
 function App() {
   return (
     <>
@@ -14,6 +16,7 @@ function App() {
               <Route path="/inicio" element={<Inicio />} />
               <Route path="/articulosfamilias" element={<ArticulosFamilias />} />
               <Route path="*" element={<Navigate to="/Inicio" replace />} />
+              <Route path="/articulos" element={<Articulos/>} />
             </Routes>
         </div>
         <Footer/>
