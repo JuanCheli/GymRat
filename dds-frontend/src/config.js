744473d6934ed314a30b758196cf116c5e20f4d2
Juan Mateo Simes <96074@sistemas.frc.utn.edu.ar>
2024-06-30 22:27:21 -0300

    Trabajo finalizado finalmente

--- dds-frontend/src/config.js
+++ dds-frontend/src/config.js
@@ -3,7 +3,7 @@ const urlServidor = "http://localhost:3000"
 const urlResourceMaquinas = urlServidor + "/api/maquinas";
 const urlResourceProveedores = urlServidor + "/api/proveedores";
 const urlResourceGimnasios = urlServidor + "/api/gimnasios";
-const urlResourceInscriptos = urlServidor + "/api/inscriptos";
+const urlResourceInscriptos = urlServidor + "/api/inscriptosJWT";
 
 
 
